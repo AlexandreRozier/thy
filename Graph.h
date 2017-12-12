ae881cdb9ccbdfa5e0d9593103a3a23b21e9b9dc
Thyriaen <thyriaen@googlemail.com>
2017-12-12 13:40:28 +0100

    test :: graph class and imports

---
+++ Graph.h
@@ -0,0 +1,15 @@
+//
+// Created by thyriaen on 12/12/17.
+//
+
+#ifndef PROJECT_GRAPH_H
+#define PROJECT_GRAPH_H
+
+class Graph {
+    int width, height;
+public:
+    Graph(int,int);
+    int area();
+};
+
+#endif //PROJECT_GRAPH_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "Graph.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    Graph myGraph(3,5);
+    std::cout << myGraph.area() << std::endl;
     return 0;
 }
